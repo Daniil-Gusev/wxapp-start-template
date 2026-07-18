522f6631ee95c9349b4588f021eb8cb401d72d47
Daniil-Gusev <i_guseva13@icloud.com>
2026-07-18 11:49:07 +0300

    refactored macos CI build

---
+++ .github/scripts/build-wx-macos.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+: "${WX_VERSION:?WX_VERSION not set}"
+: "${WITH_WEBVIEW:?WITH_WEBVIEW not set}"
+
+sudo mkdir -p /opt/wx
+sudo chown "$(id -u):$(id -g)" /opt/wx
+
+curl --retry 3 -L "https://github.com/wxWidgets/wxWidgets/releases/download/v${WX_VERSION}/wxWidgets-${WX_VERSION}.tar.bz2" \
+  | tar xj -C "$RUNNER_TEMP"
+cd "$RUNNER_TEMP/wxWidgets-${WX_VERSION}"
+mkdir build-static-release && cd build-static-release
+
+webview_opt=""
+[ "$WITH_WEBVIEW" = "true" ] && webview_opt="--enable-webview"
+
+../configure \
+  --prefix=/opt/wx \
+  --disable-shared \
+  --enable-optimise \
+  --disable-debug \
+  --disable-debug_info \
+  --disable-debug_flag \
+  --disable-tests \
+  --disable-sys-libs \
+  --with-osx_cocoa \
+  --with-macosx-version-min=10.10 \
+  --enable-universal_binary=arm64,x86_64 \
+  --with-cxx=20 \
+  --enable-mediactrl \
+  $webview_opt \
+  --enable-stc \
+  --enable-aui \
+  --enable-propgrid \
+  --enable-ribbon \
+  --enable-richtext \
+  --enable-xrc \
+  --with-opengl > /dev/null
+
+make -j"$(sysctl -n hw.ncpu)" > /dev/null
+make install > /dev/null
